5bfb03f6a4510aae8cb05adc0cf9c8998e62eab2
Borges <sdafdsf@.asdsa>
2023-11-14 19:49:34 -0300

    widget working on android < 12

--- app/src/main/java/com/pvsb/personalcostsvalidator/widget/WidgetProvider.kt
+++ app/src/main/java/com/pvsb/personalcostsvalidator/widget/WidgetProvider.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
 import com.pvsb.personalcostsvalidator.R
+import com.pvsb.personalcostsvalidator.main.MainActivity
 
 class WidgetProvider : AppWidgetProvider() {
 
@@ -25,12 +26,14 @@ class WidgetProvider : AppWidgetProvider() {
             R.layout.app_widget_main
         ).apply {
             setOnClickPendingIntent(
-                R.id.llRoot,
+                R.id.tvRegisterValue,
                 registerValuePendingIntent(context = context)
             )
+            setOnClickPendingIntent(
+                R.id.btnOpenApp,
+                openAppPendingIntent(context = context)
+            )
         }
-//
-//        setResult(RESULT_OK, Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mWidgetId))
 
         appWidgetIds?.forEach { id ->
             appWidgetManager?.updateAppWidget(id, remoteView)
@@ -47,6 +50,21 @@ class WidgetProvider : AppWidgetProvider() {
             Intent(
                 context,
                 FloatingRegisterValueActivity::class.java
+            ),
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+    }
+
+    private fun openAppPendingIntent(
+        context: Context?
+    ): PendingIntent {
+
+        return PendingIntent.getActivity(
+            context,
+            0,
+            Intent(
+                context,
+                MainActivity::class.java
             ),
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
